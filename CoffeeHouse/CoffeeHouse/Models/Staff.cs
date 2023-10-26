c248e34f168e0f244a5959c61941973c03794725
tduong1443 <144902452+tduong1443@users.noreply.github.com>
2023-10-26 13:41:36 +0700

    Update Staff.cs

--- CoffeeHouse/CoffeeHouse/Models/Staff.cs
+++ CoffeeHouse/CoffeeHouse/Models/Staff.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace CoffeeHouse.Models
 {
@@ -7,12 +8,10 @@ namespace CoffeeHouse.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public string IdSatff { get; set; }
+        public int IdSatff { get; set; }
         public string FullName { get; set; }
         public DateTime Birthday { get; set; }
         public string Address { get; set; }
-        public string UserName { get; set; }
-        public string PassWord { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Position { get; set; }
