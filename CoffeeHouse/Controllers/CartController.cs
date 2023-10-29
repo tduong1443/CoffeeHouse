73441c7491dcde67ddda5e8c60ea0db0a7b54566
tduong1443 <nguyenthuyduongk38@gmail.com>
2023-10-29 10:46:57 +0700

    First Commit

---
+++ CoffeeHouse/Controllers/CartController.cs
@@ -0,0 +1,34 @@
+﻿using CoffeeHouse.Data;
+using CoffeeHouse.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeHouse.Controllers
+{
+    public class CartController : Controller
+    {
+
+        // Add to cart
+        private CoffeeHouseContext db;
+        public CartController(CoffeeHouseContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShopCart> item = HttpContext.Session.GetJson<List<ShopCart>>("Cart") ?? new List<ShopCart>();
+            CartItem cart = new()
+            {
+                Item = item,
+                GrandTotal = item.Sum(x => x.Quantity * x.Price)
+            };
+            return View(cart);
+        }
+
+        public IActionResult Add(string id)
+        {
+            return View();
+        }
+    }
+}
---
+++ CoffeeHouse/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeeHouse.Models
+{
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Username { get; set; }
+        public string Password { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
